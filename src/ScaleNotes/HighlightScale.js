43eb26b9b4d8aaab9439812ee434882560870baa
dcmorales <dc.morales1992@gmail.com>
2019-09-20 13:57:00 -0700

    Add more highlight

--- src/ScaleNotes/HighlightScale.js
+++ src/ScaleNotes/HighlightScale.js
@@ -3,21 +3,41 @@ import React from 'react'
 class HighlightScale extends React.Component {
   componentDidMount() {
     setTimeout(() => {
-      this.renderScaleNotes()
+      this.highlightScaleNotes()
     }, 600)
   }
 
-  renderScaleNotes() {
-    const scaleNotes = this.props.renderScale()[0].concat(this.props.renderScale()[0])
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.highlightScaleNotes()
+    }, 300)
+  }
+
+  highlightScaleNotes() {
+    const scaleNotes = this.props.renderScale()[0].concat(this.props.renderScale()[1])
     scaleNotes.map(noteFull => document.getElementById(`${noteFull} slice`)
       .setAttribute('class', `${noteFull} scale-note`))
     scaleNotes.map(noteFull => document.getElementById(`${noteFull} full`)
       .setAttribute('class', `${noteFull} scale-note`))
   }
 
+  highlightNote(noteFull) {
+    setTimeout(() => {
+      this.handleNoteHighlight(noteFull, 'note')
+    }, 450)
+    this.handleNoteHighlight(noteFull, 'active')
+  }
+
+  handleNoteHighlight(noteFull, otherClassName) {
+    document.getElementById(`${noteFull} slice`)
+      .setAttribute('class', `${noteFull} ${otherClassName}`)
+    document.getElementById(`${noteFull} full`)
+      .setAttribute('class', `${noteFull} ${otherClassName}`)
+  }
+
   render() {
     return (
-      <button onClick={() => this.renderScaleNotes()}>
+      <button onClick={() => this.highlightScaleNotes()}>
         highlight
       </button>
     )
