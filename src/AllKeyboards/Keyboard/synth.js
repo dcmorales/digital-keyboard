6181b0c731af5b9934aeb1fd1546bf9cdb23db18
dcmorales <dc.morales1992@gmail.com>
2019-09-19 22:59:07 -0700

    Added sine waveform

--- src/AllKeyboards/Keyboard/synth.js
+++ src/AllKeyboards/Keyboard/synth.js
@@ -14,23 +14,29 @@ export const synth = {
       this.oscillators.type = `${waveform}`;
       this.oscillators.frequency.value = pitch;
       this.oscillators.connect(this.audioContext.destination);
-      switch (oscillator) {
-        case 'osc1':
-          this.oscillators.detune.value = -50;
-          break;
-        case 'osc2':
-          this.oscillators.detune.value = 50;
-          break;
-        case 'mid-bass':
-          this.oscillators.frequency.value = (pitch+50)/2;
-          this.oscillators.type = `${waveform}`;
-          break;
-        case 'bass':
-          this.oscillators.frequency.value = pitch/2;
-          this.oscillators.type = `${waveform}`;
-          break;
+      if (waveform === 'sine (adjusted)' && oscillator === '') {
+            this.oscillators.detune.value = 2200;
+            this.oscillators.type = 'sine'
+      } else {
+        switch (oscillator) {
+          case 'osc1':
+            this.oscillators.detune.value = -50;
+            break;
+          case 'osc2':
+            this.oscillators.detune.value = 50;
+            break;
+          case 'mid-bass':
+            this.oscillators.frequency.value = (pitch+50)/2;
+            this.oscillators.type = `${waveform}`;
+            break;
+          case 'bass':
+            this.oscillators.frequency.value = pitch/2;
+            this.oscillators.type = `${waveform}`;
+            break;
 
-        // no default
+          // no default
+        }
+
       }
       this.oscillators.start();
   }
--- src/Selections/options.js
+++ src/Selections/options.js
@@ -46,7 +46,8 @@ export const options = [
   [
     'sawtooth',
     'square',
-    'triangle'
+    'triangle',
+    'sine (adjusted)'
   ],
 
 
