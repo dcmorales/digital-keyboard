714e791a5b8bb43f5ad8b86cd07cbb3a19ed63cb
dcmorales <dc.morales1992@gmail.com>
2020-02-28 17:40:01 -0800

    Added first responsive breakpoints

--- src/components/KeyboardValuesSelected/SelectedScaleNotes/NoteDisplay.js
+++ src/components/KeyboardValuesSelected/SelectedScaleNotes/NoteDisplay.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const NoteDisplay = ({ notesPlayed }) => {
   return (
-    <div className="note-display">Notes played: {notesPlayed.join('-')}</div>
+    <div className="note-display">
+      <div className="note-display__label">Notes played:</div>
+      <div className="note-display__list">{notesPlayed.join('-')}</div>
+    </div>
   );
 };
 
