3b9ba4c3920d0acd6b883041030106ebd7b4d219
dcmorales <dc.morales1992@gmail.com>
2020-03-04 10:33:29 -0800

    Cleanup from keyboard redesign

--- src/components/KeyboardFull/Octave.js
+++ src/components/KeyboardFull/Octave.js
@@ -1,31 +1,10 @@
 import React from 'react';
 
-import FiveKeys from './FiveKeys';
-import TwoKeys from './TwoKeys';
 import Key from './Key';
 
 const Octave = ({ octNum }) => {
   return (
     <div className="octave-container">
-      {/* <FiveKeys
-        note1="C"
-        note2="Db"
-        note3="D"
-        note4="Eb"
-        note5="E"
-        octave={octNum}
-      />
-
-      <TwoKeys note1="F" note2="Gb" octave={octNum} />
-
-      <FiveKeys
-        note1="G"
-        note2="Ab"
-        note3="A"
-        note4="Bb"
-        note5="B"
-        octave={octNum}
-      /> */}
       <Key
         noteFull={`C${octNum}`}
         octave={octNum}
